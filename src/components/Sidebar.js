a1733267342797212cd65420632065fff8a822a1
Daniela <danielacardona2203@gmail.com>
2023-01-05 17:03:15 +0100

    style added to MessageForm

---
+++ src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+
+function Sidebar() {
+  const rooms = ['first room', 'second room', 'third room'];
+  return (
+    <>
+      <h2>Available rooms 💬</h2>
+      <ListGroup>
+        {rooms.map((room, idx) => (
+          <ListGroup.Item key={idx}>{room}</ListGroup.Item>
+        ))}
+      </ListGroup>
+      <h2>🧞‍♀️ Members 🧚🏽</h2>
+    </>
+  );
+}
+
+export default Sidebar;
