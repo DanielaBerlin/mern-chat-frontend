53058568b114affbc693edf3b62538d45dff895b
Daniela <danielacardona2203@gmail.com>
2023-01-06 20:13:18 +0100

    feature folder and userSlice.js added

---
+++ src/features/userSlice.js
@@ -0,0 +1,23 @@
+import {createSlice} from '@reduxjs/toolkit';
+import appApi from '../services/appApi';
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: null,
+    reducers: {
+        addNotifications: (state, { payload }) => {},
+        resetNotifications: (state, { payload }) => {},
+    },
+
+    extraReducers: (builder) => {
+        //save user after signup
+        builder.addMatcher(appApi.endpoints.signupUser.matchFullfilled, (state, { payload }) => payload);
+        // save user after login
+        builder.addMatcher(appApi.endpoints.loginUser.matchFullfilled, (state, { payload }) => payload);
+        // logout: destroy user session
+        builder.addMatcher(appApi.endpoints.logoutUser.matchFullfilled, () => null);
+    },
+});
+
+export const { addNotifications, resetNotifications } = userSlice.actions;
+export default userSlice.reducer;
