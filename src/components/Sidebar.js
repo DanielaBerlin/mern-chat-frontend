dff447b087e0d016fb9037f6ec043046219176bc
Daniela <danielacardona2203@gmail.com>
2023-01-24 17:19:17 +0100

    join room function added and styled to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -17,6 +17,20 @@ function Sidebar() {
     currentRoom,
   } = useContext(AppContext);
 
+
+  function joinRoom(room, isPublic = true) {
+    if(!user) {
+      return alert ('Please login');
+    }
+    socket.emit("join-room", room);
+    setCurrentRoom(room);
+
+    if(isPublic){
+      setPrivateMemberMsg(null)
+    }
+    //dispatch for notifications
+  }
+
   useEffect(() => {
     if (user) {
       setCurrentRoom('Berlin');
@@ -45,7 +59,8 @@ function Sidebar() {
       <h2>Available rooms 💬</h2>
       <ListGroup>
         {rooms.map((room, idx) => (
-          <ListGroup.Item key={idx}>{room}</ListGroup.Item>
+          <ListGroup.Item key={idx} onClick={()=> joinRoom(room)} active={room == currentRoom} style={{cursor: 'pointer', display: 'flex', justifyContent:'space-between'}}>
+            {room} {currentRoom !== room && <span></span>}</ListGroup.Item>
         ))}
       </ListGroup>
       <h2>🧞‍♀️ Members 🧚🏽</h2>
