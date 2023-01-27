8c4c078eae429dbe9311f27dc9cc56f4783196b9
Daniela <danielacardona2203@gmail.com>
2023-01-27 19:18:25 +0100

    fixed user picture size

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -9,10 +9,10 @@ function MessageForm() {
   const user = useSelector((state) => state.user);
   const { socket, currentRoom, setMessages, messages, privateMemberMsg } =
     useContext(AppContext);
-  const messageEndRef = useRef(null)
-    useEffect(()=>{
-      scrollToBottom();
-    }, [messages])
+  const messageEndRef = useRef(null);
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   function getFormattedDate() {
     const date = new Date();
@@ -31,8 +31,8 @@ function MessageForm() {
     e.preventDefault();
   }
 
-  function scrollToBottom(){
-    messageEndRef.current?.scrollIntoView({behavior: 'smooth'});
+  function scrollToBottom() {
+    messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   }
 
   const todayDate = getFormattedDate();
@@ -57,6 +57,25 @@ function MessageForm() {
   return (
     <>
       <div className="messages-output">
+        {user && !privateMemberMsg?._id && (
+          <div className="alert alert-info">
+            {' '}
+            Now you are participationg in the {currentRoom} room 💬
+          </div>
+        )}
+        {user && privateMemberMsg?._id && (
+          <>
+            <div className="alert alert-info conversation-info">
+              <div>
+                Your conversation with {privateMemberMsg.name}{' '}
+                <img
+                  src={privateMemberMsg.picture}
+                  className="conversation-profile-picture"
+                />
+              </div>
+            </div>
+          </>
+        )}
         {!user && <div className="alert alert-danger">Please login</div>}
         {user &&
           messages.map(({ _id: date, messagesByDate }, idx) => (
@@ -66,21 +85,39 @@ function MessageForm() {
               </p>
               {messagesByDate?.map(
                 ({ content, time, from: sender }, msgIdx) => (
-                  <div className={sender?.email == user?.email ? "message" : "incoming-message"} key={msgIdx}>
-                    <div className='message-inner'>
-                      <div className='d-flex align-items-center mb-3'>
-                        <img src={sender.picture} style={{width: 35, height: 35, objectFit: 'cover', borderRadius: '50%', marginRight: 10 }} />
-                        <p className='message-sender'>{sender._id == user?._id? "You" : sender.name}</p>
-                        </div>
-                      <p className='message-content'>{content}</p>
-                      <p className='message-timestamp-left'>{time}</p>
+                  <div
+                    className={
+                      sender?.email == user?.email
+                        ? 'message'
+                        : 'incoming-message'
+                    }
+                    key={msgIdx}
+                  >
+                    <div className="message-inner">
+                      <div className="d-flex align-items-center mb-3">
+                        <img
+                          src={sender.picture}
+                          style={{
+                            width: 35,
+                            height: 35,
+                            objectFit: 'cover',
+                            borderRadius: '50%',
+                            marginRight: 10,
+                          }}
+                        />
+                        <p className="message-sender">
+                          {sender._id == user?._id ? 'You' : sender.name}
+                        </p>
+                      </div>
+                      <p className="message-content">{content}</p>
+                      <p className="message-timestamp-left">{time}</p>
                     </div>
                   </div>
                 )
               )}
             </div>
           ))}
-          <div ref={messageEndRef}></div>
+        <div ref={messageEndRef}></div>
       </div>
       <Form onSubmit={handleSubmit}>
         <Row>
